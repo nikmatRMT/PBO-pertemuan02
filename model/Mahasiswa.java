cc52cd7e0129f782817dbb74fee1bd3a14213f08
nikmatRMT <164823503+nikmatRMT@users.noreply.github.com>
2024-03-28 00:42:05 +0800

    Memanggil banyak class di package lain

---
+++ model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilaninfo(){
+  System.out.println();
+  System.out.println("==Data Mahasiswa==========");
+  System.out.println("NPM  : 2210020047");
+  System.out.println("Nama : Nikmat Rahmatullah");
+  System.out.println("==========================");
+  }
+}
---
+++ model/MataKuliah.java
@@ -0,0 +1,11 @@
+package model;
+
+public class MataKuliah{
+ public static void tampilaninfo(){
+  System.out.println();
+  System.out.println("==Data Mata Kuliah================");
+  System.out.println("Kode : TFI3402");
+  System.out.println("Nama : Pemograman Berbasis Objek 1");
+  System.out.println("==================================");
+  }
+}

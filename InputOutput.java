827268abe84d075d4e9a24c5a031e4a8909bfc07
nikmatRMT <164823503+nikmatRMT@users.noreply.github.com>
2024-03-28 21:01:42 +0800

    Buat objek Scanner

--- InputOutput.java
+++ InputOutput.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class InputOutput{
- public static void main (String [] args){
+ public static void main(String[] args){
+  Scanner scanner = new Scanner(System.in);
+ 
  }
 }
